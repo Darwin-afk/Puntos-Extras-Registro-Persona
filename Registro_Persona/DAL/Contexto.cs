fb8456bed9b6725da958d3a57505ef0a0b5022b5
Darwin-afk <darwin_lantigua@ucne.edu.do>
2020-03-06 20:44:00 -0400

    Creacion del contexto para la entidad "Personas" usando SqLite

---
+++ Registro_Persona/DAL/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Registro_Persona.Entidad;
+using Microsoft.EntityFrameworkCore;
+
+namespace Registro_Persona.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Personas> Personas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = Database/PersonaDb.db");
+        }
+    }
+}
