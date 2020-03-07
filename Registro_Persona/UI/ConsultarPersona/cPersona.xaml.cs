313c7e576c106c44b02977e685d8c36208e60a4d
Darwin-afk <darwin_lantigua@ucne.edu.do>
2020-03-06 21:48:00 -0400

    Diseño de Consulta de Personas e implementacion de su comportamiento

---
+++ Registro_Persona/UI/ConsultarPersona/cPersona.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Linq;
+using Registro_Persona.Entidad;
+using Registro_Persona.BLL;
+
+namespace Registro_Persona.UI.ConsultarPersona
+{
+    /// <summary>
+    /// Interaction logic for cPersona.xaml
+    /// </summary>
+    public partial class cPersona : Window
+    {
+        public cPersona()
+        {
+            InitializeComponent();
+        }
+
+        private void ConsultarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var listado = new List<Personas>();
+
+            if (CriterioTextBox.Text.Trim().Length > 0)
+            {
+                listado = PersonasBLL.GetList(p => p.Nombres.Contains(CriterioTextBox.Text));
+            }
+            else
+            {
+                listado = PersonasBLL.GetList(p => true);
+            }
+
+            ConsultaDataGrid.ItemsSource = listado;
+        }
+    }
+}
