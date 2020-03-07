4d52cfd78852cf2cf191614884c67f90cf70a907
Darwin-afk <darwin_lantigua@ucne.edu.do>
2020-03-06 20:38:06 -0400

    Creacion de la entidad "Personas" con dos campos: PersonaId, Nombres.

---
+++ Registro_Persona/Entidad/Personas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace Registro_Persona.Entidad
+{
+    public class Personas
+    {
+        [Key]
+        public int PersonaId { get; set; }
+        public string Nombres { get; set; }
+
+        public Personas()
+        {
+            PersonaId = 0;
+            Nombres = string.Empty;
+        }
+    }
+}
